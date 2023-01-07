f7a555687055890ed67e7a855abc45add5772812
codersudhir <nandanesudhir1@gmail.com>
2023-01-07 13:35:21 +0530

    watchlisterror

--- pages/Watchlist.jsx
+++ pages/Watchlist.jsx
@@ -1,41 +1,11 @@
-import { Button, Flex, Image } from '@chakra-ui/react'
-import axios from 'axios'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-const Watchlist = ({data}) => {
-
-
+const Watchlist = () => {
   return (
-  
-    <div >
-      {data.map((el)=>{
-      return <Flex justifyContent={"space-around"} border="1px solid red" key={el.imdbID} >
-        <Image src={el.Images} width="200px" height={"80px"} alt="" />
-        <h1 style={{marginTop:"30px"}}>{el.Title}</h1>
-      
-       
-      </Flex>
-    })}
-
+    <div>
+      <p>Watchlist</p>
     </div>
-    
-  
   )
 }
-export async function getServerSideProps() { 
-  const res = await fetch ('http://localhost:8080/wishlist')
-  const data = await res.json();
 
-  // The next line will only be logged on the server and never on the browser console even if we make 
-  // client-side navigation.
-  // This confirms that `getServerSideProps` is guaranteed to run on the server and never on the client (or browser).
-  
-
-  return {
-      props: {
-          data
-      }
-  }
-}
 export default Watchlist
